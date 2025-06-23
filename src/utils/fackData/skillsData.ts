75435371c11bb1df3ea754ae538f67418ffbc6d1
Naeem Khan <naeemkhanpanhwar005@gmail.com>
2025-06-23 17:16:40 +0500

    skils change

--- src/utils/fackData/skillsData.ts
+++ src/utils/fackData/skillsData.ts
@@ -77,7 +77,7 @@ export const skillsData: SkillType[] = [
     },
     {
         id: 12,
-        title: "(MySQL, MS SQL Server)",
+        title: "(MySQL, MS SQL Server, MongoDB)",
         icon: FaDatabase,
         color: "#00758F"
     },
