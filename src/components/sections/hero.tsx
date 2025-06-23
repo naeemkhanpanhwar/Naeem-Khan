8eb3836323ec13b1381b2f36a8e9911623f93a79
Naeem Khan <naeemkhanpanhwar005@gmail.com>
2025-06-23 17:32:25 +0500

    Cv change

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -24,7 +24,7 @@ const Hero = () => {
                 {/* <img src="/images/shape/dot-shape-circle.png" alt="Shape" /> */}
               </div>
               <div className="hero-btns">
-                <a href="#" className="theme-btn">Hire Me <i className="far fa-angle-right" /></a>
+                <a href="#" className="theme-btn">Download CV <i className="far fa-angle-right" /></a>
               </div>
             </SlideUp>
             {/* / END HERO DESIGN AREA */}
